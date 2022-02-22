19ef50127a934e02429f8e0b11fabe90bf591eed
geunheejung <geunhee0212@gmail.com>
2022-02-22 17:06:42 +0900

    승리 조건 계산 오류 해결.

--- src/domain/calculateWinner.js
+++ src/domain/calculateWinner.js
@@ -13,13 +13,14 @@ const calculateWinner = (squares) => {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
 
+    console.log(a, b, c);
+
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      debugger;
       return squares[a];
     }
-
-    return null;
   }
+
+  return null;
 }
 
 export default calculateWinner;
